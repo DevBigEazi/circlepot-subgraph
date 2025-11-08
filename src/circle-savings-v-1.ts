46d7ea1ed44514b6685d62785b58a545416405a6
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-08 13:48:31 +0100

    feat: complete user profile mapping

---
+++ src/circle-savings-v-1.ts
@@ -0,0 +1,67 @@
+import {
+    VisibilityUpdated as VisibilityUpdatedEvent,
+    CircleCreated as CircleCreatedEvent,
+    CircleJoined as CircleJoinedEvent,
+    CircleStarted as CircleStartedEvent,
+    PayoutDistributed as PayoutDistributedEvent,
+    PositionAssigned as PositionAssignedEvent,
+    CollateralWithdrawn as CollateralWithdrawnEvent,
+    VotingInitiated as VotingInitiatedEvent,
+    VoteCast as VoteCastEvent,
+    MemberInvited as MemberInvitedEvent,
+    VoteExecuted as VoteExecutedEvent,
+    ContributionMade as ContributionMadeEvent,
+    MemberForfeited as MemberForfeitedEvent,
+} from "../generated/CircleSavingsProxy/CircleSavingsV1"
+
+export function handleVisibilityUpdated(event: VisibilityUpdatedEvent): void {
+
+}
+
+export function handleCircleCreated(event: CircleCreatedEvent): void {
+
+}
+
+export function handleCircleJoined(event: CircleJoinedEvent): void {
+
+}
+
+export function handleCircleStarted(event: CircleStartedEvent): void {
+
+}
+
+export function handlePayoutDistributed(event: PayoutDistributedEvent): void {
+
+}
+
+export function handlePositionAssigned(event: PositionAssignedEvent): void {
+
+}
+
+export function handleCollateralWithdrawn(event: CollateralWithdrawnEvent): void {
+
+}
+
+export function handleVotingInitiated(event: VotingInitiatedEvent): void {
+
+}
+
+export function handleVoteCast(event: VoteCastEvent): void {
+
+}
+
+export function handleMemberInvited(event: MemberInvitedEvent): void {
+
+}
+
+export function handleVoteExecuted(event: VoteExecutedEvent): void {
+
+}
+
+export function handleContributionMade(event: ContributionMadeEvent): void {
+
+}
+
+export function handleMemberForfeited(event: MemberForfeitedEvent): void {
+
+}
---
+++ src/personal-savings-v-1.ts
@@ -0,0 +1,15 @@
+import {
+    GoalContribution as GoalContributionEvent,
+    GoalWithdrawn as GoalWithdrawnEvent,
+    PersonalGoalCreated as PersonalGoalCreatedEvent,
+} from "../generated/PersonalSavingsProxy/PersonalSavingsV1"
+
+export function handlePersonalGoalCreated(event: PersonalGoalCreatedEvent): void { 
+
+}
+export function handleGoalContribution(event: GoalContributionEvent): void { 
+
+}
+export function handleGoalWithdrawn(event: GoalWithdrawnEvent): void { 
+
+}
